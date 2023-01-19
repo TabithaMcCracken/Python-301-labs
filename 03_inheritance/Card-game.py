44a384de904d561499ace7d3ee9b767758bc56d2
Tabitha <tabitha.r.mccracken@gmail.com>
2023-01-18 23:09:17 -0700

    Blackjack start using classes

--- 03_inheritance/Card-game.py
+++ 03_inheritance/Card-game.py
@@ -20,27 +20,54 @@ Share the game you built on your forum."""
     Queen 12
     King 13
 """
+
+import random
+import time
+
 class Card:
     """ Standard playing cards"""
-    def __init__(self, suit=0, rank=2) -> None: # default card is 2 of Clubs
+    def __init__(self, suit, rank, card_value):
         self.suit = suit
-        self.rank = rank 
+        self.rank = rank
+        self.card_value = card_value
 
-    suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-    rank_names = ['None', 'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 
-                'Jack', 'Queen', 'King']
-        
-    def __less_than__ (self, other):
-        if self.suit < other.suit: # Check suits first
-            return True
-        if self.suit>other.suit:
-            return False
+class Deck:
+    def __init__ (self):
+        self.deck = []
 
-        return self.rank<other.rank #Check rank if suits are equal
+        suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+        rank_names = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 
+                    'Jack', 'Queen', 'King']
+        card_values = {'Ace':11, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, 
+                    '9':9, '10':10, 'Jack':10, 'Queen':10, 'King':10} 
+        # Ace starts as an 11 and is changed to a 1 if hand goes over 21
+
+        for suit in suit_names:
+            for card in rank_names:
+                self.deck.append(Card(suit, card, card_values[card]))
+
+    def shuffle(self):
+        for i in range(len(self.deck)-1, 0, -1):
+            r = random.randint(0, i)
+            self.deck[i], self.deck[r] = self.deck[r], self.deck[i]
 
     def __str__(self) -> str:
-        return '%s of %s' % (Card.rank_names[self.rank], Card.suit_names[self.suit])
+        return f"{self.deck}"
 
-card1 = Card(2, 11)
-print(card1)
-    
+    def __repr__(self) -> str:
+        return f"Deck: \n{self.deck}"
+
+
+
+blackjack_deck = Deck()
+blackjack_deck.shuffle()
+print(repr(blackjack_deck))
+
+
+#class Deck():
+#    def __init__(self) -> None:
+#        self.cards = []
+#        for suit in range(4):
+#            for rank in range(1,14):
+#                card = Card(suit, rank)
+#                self.cards.append(card)
