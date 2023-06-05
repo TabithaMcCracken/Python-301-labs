8eb05c6fecc200a21514f761957e45d9612ab25d
Tabitha <tabitha.r.mccracken@gmail.com>
2023-06-05 17:47:03 -0600

    Finished exercise 5

--- 05_exceptions/05_05_check_files.py
+++ 05_exceptions/05_05_check_files.py
@@ -5,3 +5,21 @@
 # only if neither of them applies.
 
 file_name = 'integers.txt'
+line = 1
+
+try:
+
+    with open (file_name, "r") as my_file:
+        contents = my_file.readlines()[line-1]
+        contents = int(contents)
+
+except IOError:
+    print("This file does not exist.")
+
+except ValueError:
+    print("This isn't a number.")
+
+else:
+    print(f"The number is: {contents}")
+    sq_value = contents * contents
+    print(f"{contents} squared is : {sq_value}")
