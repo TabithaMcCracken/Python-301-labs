71f0f4f04aef68179a1a3562c2205b12986c9805
Tabitha <tabitha.r.mccracken@gmail.com>
2023-06-05 17:18:17 -0600

    Finished exercise 4

--- 05_exceptions/05_04_else_database.py
+++ 05_exceptions/05_04_else_database.py
@@ -2,3 +2,28 @@
 # For example, you can revisit the course module about database interactions
 # and include a try/except/else statement based on what to do whether or not
 # the database connection can be established.
+
+import sqlalchemy
+from secret import password
+import pymysql
+from pprint import pprint
+
+try:
+    engine = sqlalchemy.create_engine(f'mysql+pymysql://root:{password}@localhost/sakila')
+    connection = engine.connect()
+    metadata = sqlalchemy.MetaData()
+    print("Connection successfully made.")
+
+except Exception as ex:
+    print("Connection could not be made due to the following error: \n", ex)
+
+else:
+    #get and print Film data
+    film_table = sqlalchemy.Table('film', metadata, autoload=True, autoload_with=engine)
+
+    query = sqlalchemy.select([film_table])
+    result_proxy = connection.execute(query)
+
+    result_set = result_proxy.fetchall()
+    pprint(result_set)
+
