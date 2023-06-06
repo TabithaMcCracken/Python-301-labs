ec679631911b10f8af242f1be3d4eccaf4c20080
Tabitha <tabitha.r.mccracken@gmail.com>
2023-06-06 10:40:08 -0600

    Finished exercise 7

--- 05_exceptions/05_07_find_a_prince.py
+++ 05_exceptions/05_07_find_a_prince.py
@@ -3,3 +3,25 @@
 # like you did in the previous exercise.
 # Raise your `PrinceException()` if the first 100 characters of a book
 # contain the string "Prince".
+
+text_to_find = "Prince"
+
+book_files = [
+    "books/crime_and_punishment.txt",
+    "books/war_and_peace.txt",
+    "books/pride_and_prejudice.txt"
+]
+
+class PrinceException(Exception): pass
+
+for book in book_files:
+    with open (book, "r") as file:
+        contents = file.read(100)
+
+    try:
+        if contents.index(text_to_find):
+            raise PrinceException()
+    except ValueError:
+        print(f"The text '{text_to_find}' was not found in the first 100 characters.")
+    except PrinceException:
+        print(f"This text has the word {text_to_find} in the first 100 characters.")
