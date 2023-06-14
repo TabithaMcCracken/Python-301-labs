c06ed2eff2cdeaa7a6241fea651d399e92b28003
Tabitha <tabitha.r.mccracken@gmail.com>
2023-06-13 18:27:29 -0600

    Finished Exercise 2

--- 06_testing/06_02_mymath-tester/test_mymath.py
+++ 06_testing/06_02_mymath-tester/test_mymath.py
@@ -4,3 +4,16 @@
 # 1. Check for correct results by providing example input.
 # 2. Check that a `CustomZeroDivisionError` gets raised correctly.
 
+import unittest
+import mymath
+
+class TestMyMath(unittest.TestCase):
+    def test_subtract_divide(self):
+        self.assertEqual(mymath.subtract_divide(30,20,10), 3)
+
+class TestMyMath2(unittest.TestCase):
+    def test_subtract_divide(self):
+        self.assertEqual(mymath.subtract_divide(30,20,20), 3)
+
+if __name__ == "__main__":
+    unittest.main()
