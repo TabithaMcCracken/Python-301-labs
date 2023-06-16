ddace6565d889a2cecb02eebbb4513608a9e0ca2
Tabitha <tabitha.r.mccracken@gmail.com>
2023-06-16 17:04:00 -0600

    Finished Exercise 4

--- 06_testing/06_04_tdd_approach.py
+++ 06_testing/06_04_tdd_approach.py
@@ -9,3 +9,33 @@
 #
 # You do not need to implement the actual functions after writing the tests 
 # but of course you can do that, too.
+
+import unittest
+
+class TestMathFunctions(unittest.TestCase):
+    # Adds 2 numbers together
+    def test_add_function(self):
+        self.assertEqual(other_file.add_nums(3,5),8)
+
+    # Divides one number the other
+    def test_divide_function(self):
+        self.assertEqual(other_file.divide_nums(10,5), 2)
+
+    # Multiplies two numbers
+    def test_multiply_function(self):
+        self.assertEqual(other_file.multiply_nums(5,5), 25)
+
+
+# Sample functions
+# def add_nums (num1, num2):
+#     total = num1 + num2
+#     return total
+
+# def divide_nums (num1, num2):
+#     quotient = num1 / num2
+#     return quotient
+
+# def multiply_nums (num1, num2):
+#     product = num1 * num2
+#     return product
+
