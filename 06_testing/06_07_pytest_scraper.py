4c940dcf5f464eac37492b35e42e8235b97ab18a
Tabitha <tabitha.r.mccracken@gmail.com>
2023-07-14 15:39:37 -0600

    finished exercise 7

--- 06_testing/06_07_pytest_scraper.py
+++ 06_testing/06_07_pytest_scraper.py
@@ -1,2 +1,5 @@
 # Install `pytest` in a virtual environment and rewrite the test suite
 # for your web scraper using `pytest` instead of `unittest`.
+
+# Done
+# See test_rescrape2.py in the 06_05_recipe-scraper folder
