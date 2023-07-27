95f8ca584346de86368b6f33b7b35603a30985ff
Tabitha <tabitha.r.mccracken@gmail.com>
2023-07-26 23:12:04 -0600

    Exercise 1 and 2 in Decorators

--- 07_decorators/07_01_quote_wrap.py
+++ 07_decorators/07_01_quote_wrap.py
@@ -1,3 +1,17 @@
 # Write a decorator function that wraps text output into quotes, e.g.:
 # Hello world! ----> "Hello World!"
 # You can use it to create quotes from text output.
+
+def decorator_func(initial_func):
+    def wrapper_func(*args):
+        print('"' + words + '"')
+        return initial_func(*args)
+    return wrapper_func
+
+@decorator_func
+def prettify(words):
+    return words
+
+
+words = "Hello World"
+prettify(words)
--- 07_decorators/07_02_censorship.py
+++ 07_decorators/07_02_censorship.py
@@ -4,3 +4,17 @@
 #    "I bumped my toe! Shoot!"
 # Would, after decorating it with `@censor()`, return:
 #    "I bumped my toe! S****!"
+
+def decorator_func(initial_func):
+    def wrapper_func(*args):
+        if "Shoot" in text:
+            print(text.replace("Shoot", "S****"))
+        return initial_func(*args)
+    return wrapper_func
+
+@decorator_func
+def print_text(text):
+    return text
+
+text = "I bumped my toe! Shoot"   
+print_text(text)
