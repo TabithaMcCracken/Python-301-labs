373b8339a85657bbb9854b36190eef9baa6b3eb5
Tabitha <tabitha.r.mccracken@gmail.com>
2023-08-04 17:05:13 -0600

    Finished Exercise 3

--- 07_decorators/07_03_text_decoration.py
+++ 07_decorators/07_03_text_decoration.py
@@ -7,3 +7,18 @@
 # ******************************
 # Hello
 # ******************************
+
+def decorator_func(initial_func):
+    def wrapper_func():
+        result = initial_func()
+        line = "*" * 5
+        return f"{line}\n{result}\n{line}"
+    return wrapper_func
+
+
+
+@decorator_func
+def pretty_word():
+    return "Hello"
+
+print(pretty_word())
